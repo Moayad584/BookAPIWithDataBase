c69463e7bd8e060290e7ca9ccf56eb4ddf0cf6a5
Moayad <moayad.hakim@gmail.com>
2022-11-12 18:51:09 +0300

    Add project files.

---
+++ Domain/Entity/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+   public class Book
+    {
+        public int BookId { get; set; }
+        [Required(ErrorMessage ="Invalid Empty Name")]
+        [StringLength(50)]
+        public string BookName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string AuthorName { get; set; }
+    }
+}
---
+++ Infrastructure/RepositoryImpl/IBookRepositoryImpl.cs
@@ -0,0 +1,61 @@
+﻿using Application.Repository;
+using Domain.Entity;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.RepositoryImpl
+{
+    public class IBookRepositoryImpl : IBookRepository
+    {
+        private readonly DBaseContext Db;
+
+        public IBookRepositoryImpl (DBaseContext Db)
+        {
+            this.Db = Db;
+        }
+
+        public string AddBook(Book book)
+        { try
+            {
+                Db.Books.Add(book);
+                Db.SaveChanges();
+            }
+            
+            catch (Exception ex)
+            {
+                return book.BookName + "  Exception --> " + ex.Message;
+            }
+
+            return book.BookName;
+        }
+
+        public string DeleteBook(int Id)
+        {
+            var RemovedBook = Db.Books.FirstOrDefault(x => x.BookId == Id);
+            Db.Books.Remove(RemovedBook);
+            Db.SaveChanges();
+            return "Book has deleted";
+        }
+
+        public IList<Book> GetAll()
+        {
+            return Db.Books.ToList();
+        }
+
+        public Book GetBookById(int Id)
+        {
+            return Db.Books.FirstOrDefault(x => x.BookId == Id);
+        }
+
+        public string UpdateBook(Book book)
+        {
+            Db.Books.Update(book);
+            Db.SaveChanges();
+            return "Book has Updated";
+        }
+    }
+}
